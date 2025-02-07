9817333d441cebbf791897d403010f1b696f22e9
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-07 23:30:49 +0300

    test

--- HelloWorld6/ViewController.swift
+++ HelloWorld6/ViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: - IB Actions
 
 }
 
+// MARK: - Alert Controller
+//extension ViewController {
+//    func showAlert(withTitle title: String, andMessage message: String) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+//            self.textField.text = ""
+//        }
+//        alert.addAction(okAction)
+//        present(alert, animated: true)
+//    }
+//}
