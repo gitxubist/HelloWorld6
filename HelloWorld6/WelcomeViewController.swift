12ca2b55470dff9ad6ac34ce84ad0f6aba2511f8
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-08 23:06:23 +0300

    test

---
+++ HelloWorld6/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  HelloWorld6
+//
+//  Created by Evgeniy Morozov on 08.02.2025.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
