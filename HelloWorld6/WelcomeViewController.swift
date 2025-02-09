44166dc3cfcd48ba473b59417b520b2d97fb7aa1
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-09 14:00:39 +0300

    test

--- HelloWorld6/WelcomeViewController.swift
+++ HelloWorld6/WelcomeViewController.swift
@@ -7,9 +7,32 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
+final class WelcomeViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet private weak var greetingsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = [
+                    UIColor(red: 255/255, green: 192/255, blue: 203/255, alpha: 1.0).cgColor,
+                    UIColor(red: 135/255, green: 206/255, blue: 250/255, alpha: 1.0).cgColor  
+                ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientLayer.frame = view.bounds
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+
     }
+    
+    // MARK: - IB Actions
+    @IBAction private func logOutAction() {
+        dismiss(animated: true)
+    }
+    
 }
