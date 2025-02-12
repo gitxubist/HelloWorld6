77e498933be787893f4f8f97f3f2f185bb6881a2
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-12 22:10:25 +0300

    test

---
+++ HelloWorld6/SwiftFiles/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  HelloWorld6
+//
+//  Created by Evgeniy Morozov on 12.02.2025.
+//
+
+struct User {
+    let login: String
+    let password: String
+}
+
+struct Person {
+    let name: String
+    let familyName: String
+    let company: String
+    let department: String
+    let jobTitle: String
+}
