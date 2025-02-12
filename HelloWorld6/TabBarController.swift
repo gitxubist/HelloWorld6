5f05843460bbd9a4c0b25c692fc0cef12ddd025b
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-12 21:02:13 +0300

    test

---
+++ HelloWorld6/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarController.swift
+//  HelloWorld6
+//
+//  Created by Evgeniy Morozov on 12.02.2025.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let tabBarAppearance = UITabBarAppearance()
+//        tabBarAppearance.configureWithOpaqueBackground()
+//        
+//        tabBar.standardAppearance = tabBarAppearance
+//        tabBar.scrollEdgeAppearance = tabBarAppearance
+//    }
+}
