859537c994522b31347491763f83fa216633deb0
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-12 20:33:21 +0300

    test

---
+++ HelloWorld6/UserViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserViewController.swift
+//  HelloWorld6
+//
+//  Created by Evgeniy Morozov on 12.02.2025.
+//
+
+import UIKit
+
+final class UserViewController: UIViewController {
+    
+}
