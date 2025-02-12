13a867eed3d9a29fe2e70dee38125e912465d8c7
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-12 22:48:02 +0300

    test

--- HelloWorld6/SwiftFiles/User.swift
+++ HelloWorld6/SwiftFiles/User.swift
@@ -13,7 +13,29 @@ struct User {
 struct Person {
     let name: String
     let familyName: String
+    
+    let creedential: User
+    
+    let image: String?
+    
     let company: String
     let department: String
     let jobTitle: String
+    
+    let lifeHistory: String
+    
+    static let person: [Person] = [
+        Person(
+            name: "Evgeniy",
+            familyName: "Morozov",
+            creedential: .init(login: "1", password: "1"),
+            image: "userImage",
+            company: "company",
+            department: "situation center",
+            jobTitle: "data analyst",
+            lifeHistory: """
+            Evgeniy Morozov is a data analyst at company. 
+            He is a passionate about data science and loves to explore new technologies.
+            """)
+    ]
 }
