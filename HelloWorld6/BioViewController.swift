75925693dfadb0f9ade911ed8f983215b280155a
Морозов Евгений <evgenii.morozov@sparm.com>
2025-02-12 21:30:14 +0300

    test

---
+++ HelloWorld6/BioViewController.swift
@@ -0,0 +1,12 @@
+//
+//  BioViewController.swift
+//  HelloWorld6
+//
+//  Created by Evgeniy Morozov on 12.02.2025.
+//
+
+import UIKit
+
+final class BioViewController: UIViewController {
+    
+}
